3be9dd41666eead7d109f3bc2f19ed92db20d19a
INEX07 <46562212+INEX07@users.noreply.github.com>
2020-03-06 13:09:23 +0545

    Create help.js

---
+++ commands/General/help.js
@@ -0,0 +1,86 @@
+const Command = require("../../Base/Command");
+
+class Help extends Command {
+  constructor(client) {
+    super(client, {
+      name: "help",
+      description: "Displays bot commands.",
+      aliases: ["h", "commands"],
+      usage: ["help"]
+    });
+  }
+
+  async run(message, args, Discord) {
+    if (!args[0]) {
+      const embed = new Discord.MessageEmbed()
+        .setAuthor(`Total Commands: ${this.client.commands.size}`)
+        .setTitle(`Commands`)
+        .setThumbnail(this.client.user.displayAvatarURL({ format: "png" }))
+        .setDescription(
+          `Use **${this.client.prefix}help <command>** for more info.`
+        )
+        .addField(
+          `Fun [${
+            this.client.commands.filter(c => c.help.category === "Fun").size
+          }]`,
+          this.client.commands
+            .filter(c => c.help.category === "Fun")
+            .map(m => `\`${m.help.name}\``)
+            .join(", ")
+        )
+        .addField(
+          `General [${
+            this.client.commands.filter(c => c.help.category === "General").size
+          }]`,
+          this.client.commands
+            .filter(c => c.help.category === "General")
+            .map(m => `\`${m.help.name}\``)
+            .join(", ")
+        )
+        .addField(
+          `Music [${
+            this.client.commands.filter(c => c.help.category === "Music").size
+          }]`,
+          this.client.commands
+            .filter(c => c.help.category === "Music")
+            .map(m => `\`${m.help.name}\``)
+            .join(", ")
+        )
+        .setColor("#7289DA");
+      return message.channel.send(embed);
+    } else {
+      let cmd = this.client.fetchCommand(args[0]);
+      if (!cmd) return message.channel.send("❌ | Invalid Command.");
+      const embed = new Discord.MessageEmbed()
+        .setAuthor(
+          message.author.tag,
+          message.author.displayAvatarURL({ dynamic: true })
+        )
+        .setTitle(cmd.help.name)
+        .setThumbnail(this.client.user.displayAvatarURL({ format: "png" }))
+        .setDescription(cmd.help.description)
+        .addField("Category", cmd.help.category)
+        .addField(
+          "Aliases",
+          cmd.help.aliases.length > 0 ? cmd.help.aliases.join(", ") : "None"
+        )
+        .addField(
+          "Usage",
+          `${
+            cmd.help.usage[0].startsWith("No")
+              ? cmd.help.usage[0]
+              : cmd.help.usage.map(m => `${this.client.prefix}${m}`).join("\n")
+          }`
+        )
+        .setColor("#7289DA")
+        .setFooter(
+          `${this.client.user.username} - Commands`,
+          this.client.user.displayAvatarURL({ dynamic: true })
+        )
+        .setTimestamp();
+      return message.channel.send(embed);
+    }
+  }
+}
+
+module.exports = Help;
