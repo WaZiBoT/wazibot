ec320b7e1d423db75d08782724e0b201ac9a57e3
INEX07 <46562212+INEX07@users.noreply.github.com>
2020-03-06 13:06:58 +0545

    Create shuffle.js

---
+++ commands/Music/shuffle.js
@@ -0,0 +1,36 @@
+const Command = require("../../Base/Command");
+
+class Shuffle extends Command {
+  constructor(client) {
+    super(client, {
+      name: "shuffle",
+      description: "Shuffle the queue.",
+      aliases: [],
+      usage: ["shuffle"]
+    });
+  }
+
+  async run(message, args, Discord) {
+    let queue = this.client.queue.get(message.guild.id);
+    if (!queue) return message.channel.send("❌ | I'm not playing anything?");
+    if (!message.member.voice.channel)
+      return message.channel.send(`❌ | You're not in a voice channel!`);
+    if (
+      queue &&
+      message.guild.me.voice.channel.id !== message.member.voice.channel.id
+    )
+      return message.channel.send(`❌ | You're not in my voice channel!`);
+    if (queue.songs.length < 3)
+      return message.channel.send("❌ | You can't shuffle less than 3 songs.");
+    let songNow = queue.songs[0];
+    let arr = [];
+    arr.push(songNow);
+    queue.songs.shift();
+    let shuffled = queue.songs.shuffle();
+    arr.push(shuffled);
+    queue.songs = arr;
+    return message.channel.send("✅ | Queue shuffled!");
+  }
+}
+
+module.exports = Shuffle;
