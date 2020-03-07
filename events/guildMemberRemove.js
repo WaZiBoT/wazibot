e70b074f4640ab31b322b8adf1cb8f2af46bd014
INEX07 <46562212+INEX07@users.noreply.github.com>
2020-03-07 08:16:15 +0545

    Changed color

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -25,14 +25,14 @@ module.exports = class {
             .setMemberCount(member.guild.memberCount)
             .setGuildName("WAZIFERS")
             .setAvatar(member.user.displayAvatarURL({ dynamic: false, format: 'png', size: 1024 }))
-            .setColor("border", "#FFFF00")
-            .setColor("username-box", "#FFFF00")
-            .setColor("discriminator-box", "#FFFF00")
-            .setColor("message-box", "#FFFF00")
-            .setColor("title", "#FFFF00")
-            .setColor("avatar", "#FFFF00")
+            .setColor("border", "#FF0000")
+            .setColor("username-box", "#FF0000")
+            .setColor("discriminator-box", "#FF0000")
+            .setColor("message-box", "#FF0000")
+            .setColor("title", "#FF0000")
+            .setColor("avatar", "#FF0000")
             .setText("message", "we will miss ya!")
-            .setBackground("https://cdn.craftburg.net/stockage/img/discord/background.jpg")
+            .setBackground("https://cdn.glitch.com/6a8e40c8-fe0d-4099-99d2-02952070f31c%2Ff6ee043f-2dd6-4408-8abf-21e955453d48.image.png?v=1583548160258")
             .toAttachment();
 
         const attachment = new Discord.MessageAttachment(image.toBuffer(), "leaver.png");
